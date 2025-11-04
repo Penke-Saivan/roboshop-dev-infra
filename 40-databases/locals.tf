5a18e8be7cdc8fe377c79937f86f0b90622a7c43
Naga <pavantonystark99@gmail.com>
2025-11-04 20:06:09 +0530

    commented vpcid

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,6 +1,6 @@
 locals {
   common_name = "${var.project}- ${var.environment}"
-  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+  # vpc_id      = data.aws_ssm_parameter.vpc_id.value
   mongodb_sg_id= data.aws_ssm_parameter.mongodb_id.value
   database_subnet_id= split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
     common_tags = {
