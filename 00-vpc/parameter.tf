0560c8517100ae06ec41fbfd4a7a47dc0030900c
Naga <pavantonystark99@gmail.com>
2025-11-04 18:40:04 +0530

    added private_subnet_ids in ssm parameter store

--- 00-vpc/parameter.tf
+++ 00-vpc/parameter.tf
@@ -10,4 +10,10 @@ resource "aws_ssm_parameter" "public_subnet_ids" {
   value = join(",", module.vpc.public_subnet_ids)
 }
 
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/private_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.private_subnet_ids)
+}
+
 # stored public net is'd in the form of string list separated by comma
