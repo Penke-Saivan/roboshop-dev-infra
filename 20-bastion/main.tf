01e29d1153d9f16eb82bf543274f3af07e18ad8e
Naga <pavantonystark99@gmail.com>
2025-11-05 17:33:09 +0530

    added user data inrresource using file function

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids = [data.aws_ssm_parameter.bastion_id.value]
   subnet_id              = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
 
-
+user_data = file("bastion.sh")
   tags = merge(local.common_tags,
     { Name = "${local.common_name_suffix}- bastion" }
   )
