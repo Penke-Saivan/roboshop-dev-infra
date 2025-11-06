1c570e4e38152e2eeedc5d07648c2db0a131a04f
Naga <pavantonystark99@gmail.com>
2025-11-06 17:47:56 +0530

    Added Backend sg's - "catalogue", "user", "cart", "shipping", "payment"

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -5,5 +5,9 @@ variable "environment" {
   default = "dev"
 }
 variable "sg_name" {
-  default = ["mysql", "redis", "rabbitmq", "mongodb", "bastion", "fronted-lb", "frontend", "backend-alb"]
+  default = ["mysql", "redis", "rabbitmq", "mongodb",
+    "bastion",
+    "fronted-lb", "frontend",
+    "backend-alb",
+  "catalogue", "user", "cart", "shipping", "payment"]
 }
