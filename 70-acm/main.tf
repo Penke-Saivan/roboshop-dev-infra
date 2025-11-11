193546f8504d56220b97bc03deb41ebadf0a4b36
Naga <pavantonystark99@gmail.com>
2025-11-11 19:52:24 +0530

    **Important to understand for and for_each

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -24,6 +24,40 @@ resource "aws_acm_certificate" "roboshop" {
       type   = dvo.resource_record_type
     }
   }
+#aws_acm_certificate.roboshop.domain_validation_options is a set
+# Yes — this is a set of objects.
+
+# Each element in the set is one object representing a domain validation option.
+
+# So it’s a set<object> type, not a simple list of strings.
+
+# { for item in var.list : item => length(item) }
+
+# {
+#   "apple"  = 5
+#   "banana" = 6
+# }
+# Both create a map/object, where:
+
+# Left side of => is key
+
+# Right side is value
+
+
+# *****So now the********** for_each = {"*.believeinyou.fun" =  {
+    
+#     "name" = "_4cf8814e4dcb58943fcaa540a127f0b3.believeinyou.fun."
+#     "type" = "CNAME"
+#     "value" = "_a964e14775a2854611fa012a7f51aa79.jkddzztszm.acm-validations.aws."
+#   } }
+
+# So each value is as below 
+# "*.believeinyou.fun"(key) =  {(value)
+    
+#     "name" = "_4cf8814e4dcb58943fcaa540a127f0b3.believeinyou.fun."
+#     "type" = "CNAME"
+#     "value" = "_a964e14775a2854611fa012a7f51aa79.jkddzztszm.acm-validations.aws."
+#   }
 
   allow_overwrite = true
   name            = each.value.name
@@ -33,8 +67,22 @@ resource "aws_acm_certificate" "roboshop" {
   zone_id         = var.zone_id
 }
 
+
+# aws_acm_certificate.roboshop.domain_validation_options = toset([
+#   {
+#     "domain_name" = "*.believeinyou.fun"
+#     "resource_record_name" = "_4cf8814e4dcb58943fcaa540a127f0b3.believeinyou.fun."
+#     "resource_record_type" = "CNAME"
+#     "resource_record_value" = "_a964e14775a2854611fa012a7f51aa79.jkddzztszm.acm-validations.aws."
+#   },
+# ])
+
 #Now validating - like hitting validation button to validate
 resource "aws_acm_certificate_validation" "roboshop" {
   certificate_arn         = aws_acm_certificate.roboshop.arn
   validation_record_fqdns = [for record in aws_route53_record.roboshop : record.fqdn]
+#   [for item in var.list : item.upper()]
+# If var.list = ["a", "b", "c"],
+# ✅ Result → ["A", "B", "C"]
+
 }
