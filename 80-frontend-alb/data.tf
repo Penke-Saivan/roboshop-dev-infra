b9982306e94aeae3994a6e0fd386706a3d2cb4fc
Naga <pavantonystark99@gmail.com>
2025-11-11 19:51:57 +0530

    dup- backend-alb-sg-id-retrieving

--- 80-frontend-alb/data.tf
+++ 80-frontend-alb/data.tf
@@ -7,9 +7,9 @@ data "aws_ssm_parameter" "bastion_id" {
 data "aws_ssm_parameter" "backend-alb_sg-id" {
   name = "/${var.project}/${var.environment}/backend-alb_sg-id" #/roboshop/dev/backend-alb_sg-id
 }
-data "aws_ssm_parameter" "backend-alb_sg-id" {
-  name = "/${var.project}/${var.environment}/backend-alb_sg-id" #/roboshop/dev/backend-alb_sg-id
-}
+# data "aws_ssm_parameter" "backend-alb_sg-id" {
+#   name = "/${var.project}/${var.environment}/backend-alb_sg-id" #/roboshop/dev/backend-alb_sg-id
+# }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project}/${var.environment}/private_subnet_ids"
