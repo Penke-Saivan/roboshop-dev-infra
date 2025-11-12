347654549f06afb06df105cb69b16f0420114313
Naga <pavantonystark99@gmail.com>
2025-11-12 13:29:57 +0530

    changed the frontend_alb name in aws_lb_listener

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -18,7 +18,7 @@ resource "aws_lb" "frontend_alb" {
   )
 }
 
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "frontend_alb" {
   load_balancer_arn = aws_lb.frontend_alb.arn
   port              = "443"
   protocol          = "HTTPS"
