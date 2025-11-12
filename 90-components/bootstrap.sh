7cf48865070484d6ded5658074d338f8f7703dd5
Naga <pavantonystark99@gmail.com>
2025-11-12 13:31:53 +0530

    using - the module which was created - terraform-roboshop-component

---
+++ 90-components/bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+component=$1
+environment=$2
+dnf install ansible -y
+# ansible-pull -U "https://github.com/Penke-Saivan/ansible-roboshop-roles-Terraform.git" -e component=$component main.yaml
+
+#how to copy a file from terraform to ec2
+
+REPO_URL=https://github.com/Penke-Saivan/ansible-roboshop-roles-Terraform.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roboshop-roles-Terraform
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop/
+
+# touch ansible.log
+
+cd $REPO_DIR
+
+if [ -d $ANSIBLE_DIR ]; then
+#check if repo already exists
+    cd $ANSIBLE_DIR
+    git pull
+
+else
+
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+
+fi
+echo "environment is $2"
+ansible-playbook -e component=$component -e envir=$environment main.yaml
+
