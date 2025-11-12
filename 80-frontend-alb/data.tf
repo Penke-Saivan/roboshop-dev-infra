9b4fd94622a75f91b6596fb50667cc08a03639c1
Naga <pavantonystark99@gmail.com>
2025-11-12 18:41:08 +0530

    backend-alb chaging ref

--- 80-frontend-alb/data.tf
+++ 80-frontend-alb/data.tf
@@ -5,7 +5,7 @@ data "aws_ssm_parameter" "bastion_id" {
   name = "/${var.project}/${var.environment}/bastion_sg-id"
 }
 data "aws_ssm_parameter" "backend-alb_sg-id" {
-  name = "/${var.project}/${var.environment}/backend-alb_sg-id" #/roboshop/dev/backend-alb_sg-id
+  name = "/${var.project}/${var.environment}/backend_alb_sg-id" #/roboshop/dev/backend-alb_sg-id
 }
 # data "aws_ssm_parameter" "backend-alb_sg-id" {
 #   name = "/${var.project}/${var.environment}/backend-alb_sg-id" #/roboshop/dev/backend-alb_sg-id
@@ -16,7 +16,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
 }
 
 data "aws_ssm_parameter" "frontend-alb_sg-id" {
-  name = "/${var.project}/${var.environment}/fronted-lb_sg-id" #/roboshop/dev/backend-alb_sg-id
+  name = "/${var.project}/${var.environment}/frontend_alb_sg-id" #/roboshop/dev/backend-alb_sg-id
 }
 data "aws_ssm_parameter" "frontend_alb_certificate_arn" {
   name = "/${var.project}/${var.environment}/frontend_alb_certificate_arn"
