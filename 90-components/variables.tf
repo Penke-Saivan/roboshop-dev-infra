06cb0dea5808a8980f799a72490ad0bc757ebe9b
Naga <pavantonystark99@gmail.com>
2025-11-15 18:31:08 +0530

    only verifying payment

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -10,15 +10,15 @@ variable "components" {
     #   rule_priority = 10
     #   #if you want to add any input ike instance type we can add from here
     # }
-    user = {
-      rule_priority = 20
-    }
-    cart = {
-      rule_priority = 30
-    }
-    shipping = {
-      rule_priority = 40
-    }
+    # user = {
+    #   rule_priority = 20
+    # }
+    # cart = {
+    #   rule_priority = 30
+    # }
+    # shipping = {
+    #   rule_priority = 40
+    # }
     payment = {
       rule_priority = 50
     }
