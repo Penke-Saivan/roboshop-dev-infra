083d27e41db1a48cc00a9c9b3839bb0416593267
Naga <pavantonystark99@gmail.com>
2025-11-15 17:17:51 +0530

    cart added in the loop along with user

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -13,9 +13,9 @@ variable "components" {
     user = {
       rule_priority = 20
     }
-    # cart = {
-    #   rule_priority = 30
-    # }
+    cart = {
+      rule_priority = 30
+    }
     # shipping = {
     #   rule_priority = 40
     # }
