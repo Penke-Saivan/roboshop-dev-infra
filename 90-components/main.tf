ae2991d740967272486b83489875424ba6f2673f
Naga <pavantonystark99@gmail.com>
2025-11-15 11:39:19 +0530

    Looping components passing rule_priority in 90-components

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,12 @@
+# module "components" {
+#   source = "../../terraform-roboshop-component"
+#   component = var.component
+#   rule_priority = var.rule_priority
+# }
 module "components" {
-  source = "../../terraform-roboshop-component"
-  component = var.component
-  rule_priority = var.rule_priority
+  #looping components
+  for_each      = var.components
+  source        = "git::https://github.com/Penke-Saivan/terraform-roboshop-component.git?ref=main"
+  component     = each.key
+  rule_priority = each.value.rule_priority
 }
--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -4,3 +4,26 @@ variable "component" {
 variable "rule_priority" {
   default = 10
 }
+variable "components" {
+  default = {
+    catalogue = {
+      rule_priority = 10
+      #if you want to add any input ike instance type we can add from here
+    }
+    user = {
+      rule_priority = 20
+    }
+    cart = {
+      rule_priority = 30
+    }
+    shipping = {
+      rule_priority = 40
+    }
+    payment = {
+      rule_priority = 50
+    }
+    frontend = {
+      rule_priority = 10 #Frontend load balancer is different
+    }
+  }
+}
