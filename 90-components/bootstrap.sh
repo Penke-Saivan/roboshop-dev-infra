a42505e4a2d59c719f236abc1628edde1f7e3260
Naga <pavantonystark99@gmail.com>
2025-11-16 18:56:26 +0530

    almost there

--- 90-components/bootstrap.sh
+++ 90-components/bootstrap.sh
@@ -2,19 +2,27 @@
 
 component=$1
 environment=$2
-dnf install ansible -y 
-#REMOTE-EXEC SCRIPT
-sudo mkdir -p /etc/ansible
+# dnf install ansible -y 
+# #REMOTE-EXEC SCRIPT
+# sudo mkdir -p /etc/ansible
 
-sudo tee /etc/ansible/ansible.cfg > /dev/null << 'EOF'
-[defaults]
-transport = ssh
+# sudo tee /etc/ansible/ansible.cfg > /dev/null << 'EOF'
+# [defaults]
+# transport = ssh
 
-[ssh_connection]
-ssh_args =
-ssh_executable = /usr/bin/ssh
-pipelining = True
-EOF
+# [ssh_connection]
+# ssh_args =
+# ssh_executable = /usr/bin/ssh
+# pipelining = True
+# EOF
+
+echo "=== Fixing OpenSSL mismatch ==="
+dnf install -y openssl openssl-libs openssh openssh-server openssh-clients || true
+dnf install -y python3-devel gcc || true
+
+echo "=== Installing Ansible ==="
+dnf install ansible -y
+
 
 # ansible-pull -U "https://github.com/Penke-Saivan/ansible-roboshop-roles-Terraform.git" -e component=$component main.yaml
 
@@ -44,6 +52,16 @@ else
 fi
 
 echo "Component: $component | Environment: $environment"
+#  inventory with correct settings
+cat <<EOF > inventory.ini
+[local]
+localhost
+
+[all:vars]
+ansible_connection=local
+ansible_python_interpreter=/usr/bin/python3
+EOF
+
 
 echo "-------------------------------------environment is $2"
 ansible-playbook -e component=$component -e envir=$environment main.yaml
